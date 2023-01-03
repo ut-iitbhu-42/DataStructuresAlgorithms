ecf360ba38423b144bf9738cd5f4dcfc2bdc1439
Ravisha Sethi <ravishasethi@Ravishas-MacBook-Air.local>
2023-01-03 22:42:17 +0530

    intial setup

--- src/dsa/graph/Creation.java
+++ src/dsa/graph/Creation.java
@@ -3,5 +3,7 @@ package dsa.graph;
 public class Creation {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("first java project");
     }
+
 }
